5f8cb1cb15d8ccbbed247bcad3294044c3d59f99
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-01-24 22:07:27 -0300

    logging unhandled errors to the console

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -12,5 +12,7 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error(err);
+
   res.status(400).send({ errors: [{ message: err.message }] });
 };
