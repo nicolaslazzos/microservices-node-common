0f6df589b77b4298d2923175e0317a7161975a7b
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-01-30 22:12:40 -0300

    adding some events for the orders

---
+++ src/events/order-cancelled-event.ts
@@ -0,0 +1,15 @@
+import { Subjects } from "./subjects";
+
+// the event needs the orderId for updating the payment service
+
+// the event needs the ticketId for the ticket service to update the ticket
+
+export interface OrderCancelledEvent {
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    ticket: {
+      id: string;
+    };
+  };
+}
---
+++ src/events/order-created-event.ts
@@ -0,0 +1,24 @@
+import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/order-status";
+
+// the event needs the expiration date for the expiration service
+
+// the event needs the orderId, the userId and the price for the payment service
+
+// the event needs the ticketId for the ticket service to update the ticket
+
+// the order status was added just in case...
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
